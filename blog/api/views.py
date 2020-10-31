6e6786fbc15cf89504d55985c7a0cf1079684b14
otumian-empire <popecan1000@gmail.com>
2020-10-31 15:17:39 +0000

    CRUD post

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
-from .models import Category, Blog
-from .serializers import PostSerializer, CategorySerializer
+from rest_framework.response import Response
+
+from .models import Blog, Category
+from .serializers import CategorySerializer, PostSerializer
 
 
 # Create your views here.
@@ -15,3 +19,48 @@ def index(req):
     posts = Blog.objects.all()
     serializer = PostSerializer(posts, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+@csrf_exempt
+def create_post(request):
+    try:
+        if request.method == 'POST':
+            serializer = PostSerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    except Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@csrf_exempt
+def post_detail(request, pk):
+    """
+    Retrieve, update or delete a post.
+    """
+    try:
+        post = Blog.objects.get(pk=pk)
+    except Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
