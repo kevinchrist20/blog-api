3471a11288213d1cc96e9b796c119dca8db506b0
otumian-empire <popecan1000@gmail.com>
2020-10-31 11:15:11 +0000

    Add User as foreign key

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
 from django.utils import timezone
 
 
@@ -11,9 +13,9 @@ class Category(models.Model):
 class Blog(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     content = models.models.TextField()
     date_create = models.DateTimeField(default=timezone.now)
-    category = provider = models.ForeignKey(
+    category = models.ForeignKey(
         Category, on_delete=models.DO_NOTHING)
     date_updated = models.DateTimeField(auto_now=True)
