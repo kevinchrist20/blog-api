7f371c5c27db19abd85e492534b876fdc61d0fc0
kevinchrist20 <kevinamevor@outlook.com>
2020-10-31 15:22:10 +0000

    Added new views to url patterns

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("categories/", views.Categories.as_view(), name="categories"),
+    path("categories/<int:pk>/", views.CategoryDetail.as_view(), name="category")
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
