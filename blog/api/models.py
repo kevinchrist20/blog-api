217040a6133a1f818f368368548280cdd0d1ae3a
otumian-empire <popecan1000@gmail.com>
2020-10-31 11:01:23 +0000

    DB setup

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+
+class Blog(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    content = models.models.TextField()
+    date_create = models.DateTimeField(default=timezone.now)
+    category = provider = models.ForeignKey(
+        Category, on_delete=models.DO_NOTHING)
+    date_updated = models.DateTimeField(auto_now=True)
